eef3f983ccfe40ce4b370a03fc5bb15a7c45a7cf
Shell <104874910+Shell4026@users.noreply.github.com>
2026-03-09 15:28:20 +0900

    feat: Implement PlayerSoundController, Add jump sound

---
+++ Source/client/Player/PlayerSoundController.cpp
@@ -0,0 +1,31 @@
+﻿#include "Player/PlayerSoundController.h"
+#include "Player/PlayerMovement.h"
+
+namespace sh::game
+{
+	PlayerSoundController::PlayerSoundController(GameObject& owner) :
+		Component(owner)
+	{
+	}
+	SH_USER_API void PlayerSoundController::Awake()
+	{
+		if (player == nullptr)
+			SH_ERROR("player is nullptr!");
+		if (audioSource == nullptr)
+			SH_ERROR("audioSource is nullptr!");
+
+		if (jumpSound == nullptr)
+			SH_ERROR("jumpSound is nullptr!");
+	}
+	SH_USER_API void PlayerSoundController::FixedUpdate()
+	{
+		if (player->IsLocal())
+		{
+			if (jumpSound != nullptr && player->GetMovement()->IsJumpTriggered())
+			{
+				SH_INFO("jump sound");
+				audioSource->PlayOneshot(*jumpSound);
+			}
+		}
+	}
+}//namespace
---
+++ Source/include/Player/PlayerSoundController.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+#include "Export.h"
+#include "Game/Component/Component.h"
+#include "Game/Component/Sound/AudioSource.h"
+#include "Sound/SoundClip.h"
+
+namespace sh::game
+{
+	class Player;
+	class PlayerSoundController : public Component
+	{
+		COMPONENT(PlayerSoundController, "user")
+	public:
+		SH_USER_API PlayerSoundController(GameObject& owner);
+
+		SH_USER_API void Awake() override;
+		SH_USER_API void FixedUpdate() override;
+
+		SH_USER_API auto GetPlayer() const -> Player& { return *player; }
+		SH_USER_API auto GetAudioSource() const -> AudioSource& { return *audioSource; }
+		SH_USER_API auto GetJumpSound() const -> sound::SoundClip* { return jumpSound; }
+	private:
+		PROPERTY(player, core::PropertyOption::sobjPtr)
+		Player* player = nullptr;
+		PROPERTY(audioSource)
+		AudioSource* audioSource = nullptr;
+		PROPERTY(jumpSound)
+		sound::SoundClip* jumpSound = nullptr;
+	};
+}//namespace
